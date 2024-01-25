60bb8a2427c65d7fe386ab8b7e0886621428a981
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2024-01-25 15:35:21 +0000

    feat: added methods, index, and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Contact Management Notes
+
+* Database information is in the config file. I used PHPStorm to open it up and write queries to
+create the database contact_managment ( I am aware that there is a typo as well) and then the list_contact 
+table with data.
+
+### Methods that are included in the contactManager file:
+
+* List all emails with the most recently added one first
+* Add an email address without reloading the page
+* Delete an email address without reloading the page
+
+The index.html file is a good start but I could not finish it in 2 hours. 
--- contactManager.php
+++ contactManager.php
@@ -1,1 +1,67 @@
 <?php
+
+include 'config.php';
+
+/**
+ * View a list of email addresses in the mailing list, most recently added first
+ */
+function getEmailList()
+{
+    global $conn;
+    $sql = "SELECT * FROM list_contact ORDER BY created_at DESC";
+    $result = $conn->query($sql);
+
+    $emails = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $emails[] = $row;
+        }
+    }
+
+    return $emails;
+}
+
+
+/**
+ * Add a new email address to the list without reloading the page
+ */
+function addEmailAddress($email, $name) {
+    global $conn;
+    $stmt = $conn->prepare("INSERT INTO list_contact (email_address, name) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $name);
+
+    $result = $stmt->execute();
+
+    $stmt->close(); /
+
+    if ($result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+/**
+ * Delete an email address from the list without reloading the page
+ */
+function deleteEmailAddress($email) {
+    global $conn;
+
+    $stmt = $conn->prepare("DELETE FROM list_contact WHERE email_address = ?");
+
+    $stmt->bind_param("s", $email);
+
+    $result = $stmt->execute();
+
+    $stmt->close();
+
+    return $result;
+}
+
+// Another possible function to add below
+
+/**
+ * Update an email address from the list without reloading the page
+ */
+
